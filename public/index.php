abdbe6cff06bd17ac084bff96582d155412b579b
Rodrigo Thimoteo <rodrigomoteo@gmail.com>
2021-07-01 09:01:37 -0300

    Correção no nome da folha de estilos

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CEP e Hora</title>
-    <link rel="stylesheet" href="/styles/stykes.css">
+    <link rel="stylesheet" href="/styles/styles.css">
     <link rel="stylesheet" href="/styles/global.css">
     
 </head>
